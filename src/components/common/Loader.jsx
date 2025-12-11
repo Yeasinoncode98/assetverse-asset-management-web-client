9fda2a45e8e99aa4e2352dec54926c3df3072e7c
Yeasinoncode98 <devoncode98@gmail.com>
2025-12-12 00:11:44 +0600

    Added loader part

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,5 +1,17 @@
+// export default function Loader() {
+//   return (
+//     <div className="h-40 flex items-center justify-center">
+//       <span className="loading loading-bars loading-xl"></span>
+//     </div>
+//   );
+// }
+
+// 2.................
+
 export default function Loader() {
   return (
-    <div className="h-40 flex items-center justify-center">Loading...</div>
+    <div className="fixed inset-0 flex items-center justify-center">
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
   );
 }
